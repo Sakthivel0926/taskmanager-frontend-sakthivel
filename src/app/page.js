84507eddd369f5611aa307f3cd442c9ad4a05ae5
Sakthivel <sakthivel.c2022cse@sece.ac.in>
2026-05-23 11:52:19 +0530

    Error in the page.js is fixed

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,31 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Task Manager</h1>
+    <main className="min-h-screen flex flex-col items-center justify-center gap-4">
+      <h1 className="text-4xl font-bold">
+        Task Manager
+      </h1>
 
-      <Link href="/login">Login</Link>
-      <br />
-      <Link href="/register">Register</Link>
-      <br />
-      <Link href="/tasks">Tasks</Link>
-    </div>
+      <Link
+        href="/login"
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Login
+      </Link>
+
+      <Link
+        href="/register"
+        className="bg-green-500 text-white px-4 py-2 rounded"
+      >
+        Register
+      </Link>
+
+      <Link
+        href="/tasks"
+        className="bg-black text-white px-4 py-2 rounded"
+      >
+        Tasks
+      </Link>
+    </main>
   );
 }
