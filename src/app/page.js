96c7bb768c98b8657743d2efa8a3c9783ddd10ef
Sakthivel <sakthivel.c2022cse@sece.ac.in>
2026-05-23 11:41:53 +0530

    Error in the page.js is fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,7 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-export default function HomePage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      router.push("/tasks");
-    } else {
-      router.push("/login");
-    }
-  }, []);
-
+export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold">
-        Loading...
-      </h1>
+    <div>
+      Home Page
     </div>
   );
 }
