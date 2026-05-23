faff81c7e8e2041c8cd3ba396aa1ee729871275e
Sakthivel <sakthivel.c2022cse@sece.ac.in>
2026-05-23 11:47:47 +0530

    Error in the page.js is fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      Home Page
+      <h1>Task Manager</h1>
+
+      <Link href="/login">Login</Link>
+      <br />
+      <Link href="/register">Register</Link>
+      <br />
+      <Link href="/tasks">Tasks</Link>
     </div>
   );
 }
